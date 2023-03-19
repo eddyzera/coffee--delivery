a4af7865c498723547e012bedf3d3c1fed9aa7f2
ednestig <edgar@nestig.com>
2023-03-19 19:35:22 -0300

    feat: create CoffeeList component

---
+++ src/components/Step/Homepage/CoffeeList/CoffeeList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const CoffeeList: React.FunctionComponent = () => {
+  return (
+    <section className="pt-[32px] pb-[157px] px-[160px]">
+      <h2 className="text-8 text-brown-500 leading-1">Nossos cafés</h2>
+      <div></div>
+    </section>
+  )
+}
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from '../../components'
 import { SectionBanner } from '../../components/Step/Homepage'
+import { CoffeeList } from '../../components/Step/Homepage/CoffeeList/CoffeeList'
 
 export const HomePage: React.FunctionComponent = () => {
   return (
     <>
       <Header />
       <SectionBanner />
+      <CoffeeList />
     </>
   )
 }
