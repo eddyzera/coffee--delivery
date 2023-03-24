0276adde252064b6e1587067d1b4e79bd5c952bd
ednestig <edgar@nestig.com>
2023-03-24 12:09:25 -0300

    chore: add json server

--- src/components/Step/Homepage/CoffeeList/CoffeeList.tsx
+++ src/components/Step/Homepage/CoffeeList/CoffeeList.tsx
@@ -9,7 +9,7 @@ export const CoffeeList: React.FunctionComponent = () => {
     >
       <h2 className="text-8 text-brown-500 leading-1">Nossos cafés</h2>
       <div className="grid grid-cols-4 gap-x-[40px] gap-y-[32px] mt-[54px]">
-        {[...Array(15).keys()].map((it) => (
+        {[...Array(14).keys()].map((it) => (
           <CoffeeCardItem key={it} />
         ))}
       </div>
